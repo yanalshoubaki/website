2064788a1b6c312566d140ee621c55c2be77fd54
Yanal Shoubaki <yanalshoubaki233@gmail.com>
2022-12-31 11:51:34 +0300

    Update next config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@ const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true, // Recommended for the `pages` directory, default in `app`.
+  swcMinify: true,
   experimental: {
+    // Required:
     appDir: true,
   },
   webpack: (config, { isServer }) => {
